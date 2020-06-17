7929a3a04c3b7f2431e5e69ef9616de9c0e75f6e
Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>
2020-06-17 21:42:52 +1000

    Code Cleaning
    Cleaning code, making errors more reportable and handling them better.

--- script.py
+++ script.py
@@ -14,35 +14,55 @@ sheet = workbook[sheetName]
 
 # Set first Cell
 i = 2
+counter = 0
 cell = sheet['A' + str(i)].value
 
+# Construct URL for Search
+def getURL(i):
+    try: 
+        query = (sheet['A' + str(i)].value + " " + sheet['B' + str(i)].value).split()
+        url = "http://www.google.com/search?hl=en&q="
+        for term in query:
+            url += term + "%20"
+        url += "phone%20number"
+        return url
+    except:
+        raise TypeError("Check characters, error obtaining URL for")
+
+# Execute Search with Chromium Agent
+def runSearch(url):
+    try: 
+        hdr = {'User-Agent': 'Chrome/70.0.3538.77'}
+        req = Request(url, headers=hdr)
+        page = urlopen(req)
+        return BeautifulSoup(page, "html.parser")
+    except:
+        raise LookupError("Check characters, unable to complete search for")
+
+# Find phone number and save in doc
+def returnNumber(i, searchSoup):
+    try:
+        number = searchSoup.find_all("div", attrs={"class": "BNeawe iBp4i AP7Wnd"})[1].getText()
+        return number
+    except:
+        raise LookupError("Check terms, unable to find phone number from search for")
+
 # Loop through column
-while cell != None:
-    
-    # Construct URL for Search
-    query = (sheet['A' + str(i)].value + " " + sheet['B' + str(i)].value).split()
-    url = "http://www.google.com/search?hl=en&q="
-    for term in query:
-        url += term + "%20"
-    url += "phone%20number"
-
-    # Execute Search with Mozilla Agent
-    hdr = {'User-Agent': 'Chrome/70.0.3538.77'}
-    req = Request(url, headers=hdr)
-    page = urlopen(req)
-    soup = BeautifulSoup(page, "html.parser")
-
-    # FOR DEBUG
-    #print(soup.prettify())
-
-    # Find phone number and save in doc
-    number = soup.find("div", attrs={"class": "BNeawe iBp4i AP7Wnd"}).getText()
-    sheet['E' + str(i)] = number
-
-    # Iterate
-    i += 1
-    cell = sheet['A' + str(i)].value
+while cell != None: 
+    try:
+        url = getURL(i)
+        searchSoup = runSearch(url)
+        number = returnNumber(i, searchSoup)
+        sheet['E' + str(i)] = number
+        print("Found for: " + cell)
+        counter += 1
+    except Exception as e:
+        print(str(e) + ": " + cell)
+    finally:
+        # Iterate
+        i += 1
+        cell = sheet['A' + str(i)].value
 
 # Save XLSX
-workbook.save('test.xlsx')
-print("Done! " + str(i-2) + " phone numbers added...")
+workbook.save(workbookName)
+print("Done! " + str(counter) + " phone numbers added...")
